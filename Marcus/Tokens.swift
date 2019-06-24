24ceb6a925943f22266bd865d43d4fa161bcea7b
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-06-24 18:16:06 +0300

    Actually, whitespace is a terribly useless token

--- Marcus/Tokens.swift
+++ Marcus/Tokens.swift
@@ -16,25 +16,37 @@ let tokenMap: [Character: Token] = [
     "}" : .curlyBrackets(.close),
     "(" : .roundBrackets(.open),
     ")" : .roundBrackets(.close),
-    ":" : .colon,
-    " " : .whitespace
+    ":" : .colon
 ]
 
 func tokenize(_ string: String) -> [Token] {
     var tokens = [Token]()
     for char in string {
-        if let ezToken = tokenMap[char] {
-            tokens.append(ezToken)
-        } else if let latest = tokens.last {
-            switch latest {
-            case .label(let oldString):
-                _ = tokens.removeLast()
-                tokens.append(.label(oldString + String(char)))
-            case .colon, .curlyBrackets, .roundBrackets, .whitespace:
+        switch char {
+        case "{":
+            tokens.append(.curlyBrackets(.open))
+        case "}":
+            tokens.append(.curlyBrackets(.close))
+        case "(":
+            tokens.append(.roundBrackets(.open))
+        case ")":
+            tokens.append(.roundBrackets(.close))
+        case ":":
+            tokens.append(.colon)
+        case " ":
+            break
+        default:
+            if let latest = tokens.last {
+                switch latest {
+                case .label(let oldString):
+                    _ = tokens.removeLast()
+                    tokens.append(.label(oldString + String(char)))
+                case .colon, .curlyBrackets, .roundBrackets, .whitespace:
+                    tokens.append(.label(String(char)))
+                }
+            } else {
                 tokens.append(.label(String(char)))
             }
-        } else {
-            tokens.append(.label(String(char)))
         }
     }
     return tokens
